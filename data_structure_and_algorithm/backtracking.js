6e36659ffd1e0217cbe68fe5863834edb6e7b202
Allen Lin <allenlin113@gmail.com>
2023-06-12 15:05:24 -0700

    Add Dynamic Programming section to README.me and backtracking formula

---
+++ data_structure_and_algorithm/backtracking.js
@@ -0,0 +1,33 @@
+/* 
+	State 
+	
+*/
+const isValidState = (state) => {
+    // Check if it is a valid solution
+    return true;
+};
+
+const getCandidates = (state) => {
+    return [];
+};
+
+const search = (state, solutions) => {
+    if (isValidState(state)) {
+        // solutions.push(state.copy); python
+		// solutions.push([...state]); js
+        // return
+    }
+
+    for (let candidate of getCandidates(state)) {
+        // state.add(candidate);
+        // search(state, solutions);
+        // state.remove(candidate);
+    }
+};
+
+const solve = () => {
+    const solutions = [];
+    const state = new Set();
+    search(state, solutions);
+    return solutions;
+};
