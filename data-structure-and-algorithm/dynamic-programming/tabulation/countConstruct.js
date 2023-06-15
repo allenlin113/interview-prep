304654bbcb820b89695bf3b3bfe335bcdab39792
Allen Lin <allenlin113@gmail.com>
2023-06-15 14:55:57 -0700

    Add tabulation examples to Dynamic Programming repo

---
+++ data-structure-and-algorithm/dynamic-programming/tabulation/countConstruct.js
@@ -0,0 +1,32 @@
+/* 
+	https://leetcode.com/problems/word-break/
+*/
+
+const countConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+
+    for (let i = 0; i <= target.length; i++) {
+        for (let word of wordBank) {
+            if (target.slice(i, i + word.length) === word) {
+                table[i + word.length]+= table[i]
+            }
+        }
+    }
+    return table[target.length];
+};
+
+console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"])); // 2
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def"])); // 1
+console.log(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])); // 0
+console.log(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])); // 4
+console.log(
+    countConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz", [
+        "a",
+        "aa",
+        "aaa",
+        "aaaa",
+        "aaaaa",
+        "aaaaaa",
+    ])
+); // 0
